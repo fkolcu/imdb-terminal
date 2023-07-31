39890e26ca3c7847901fd564e43ae19617179c97
fkolcu <furkankolcu@hotmail.com.tr>
2023-07-31 18:32:43 +0300

    modal creator

---
+++ internal/modal.go
@@ -0,0 +1,34 @@
+package internal
+
+import "github.com/rivo/tview"
+
+const (
+	ButtonOk int = iota
+)
+
+type ModalConfig struct {
+	Text       string
+	ButtonType int
+	EventOk    func()
+}
+
+func NewModal(config ModalConfig) *tview.Modal {
+	myModal := tview.NewModal()
+	myModal.SetText(config.Text)
+
+	var buttons []string
+
+	switch config.ButtonType {
+	case ButtonOk:
+		buttons = []string{"OK"}
+	}
+
+	myModal.AddButtons(buttons)
+	myModal.SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+		if buttonLabel == "OK" {
+			config.EventOk()
+		}
+	})
+
+	return myModal
+}
