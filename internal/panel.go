ff261f2ad5a96984d4be6a8a4596f43d58e92bdb
fkolcu <furkankolcu@hotmail.com.tr>
2023-07-31 17:56:10 +0300

    search imdb and view result on the list

--- internal/panel.go
+++ internal/panel.go
@@ -6,6 +6,10 @@ import (
 	"github.com/rivo/tview"
 )
 
+var tab1 *tview.InputField
+var tab2 *tview.List
+var tab3 *tview.List
+
 type Tab interface {
 	tview.Primitive
 	SetBorderColor(color tcell.Color) *tview.Box
@@ -57,15 +61,16 @@ func DrawPanel() Panel {
 	grid.SetColumns(-1, -1)
 
 	// Row 1 Column 1-2
-	tab1 := buildSearch()
+	tab1 = buildSearch()
+	tab1.SetDoneFunc(onSearched)
 	grid.AddItem(tab1, 0, 0, 1, 2, 0, 0, true)
 
 	// Row 2 Column 1
-	tab2 := buildTitles([]ListItem{})
+	tab2 = buildTitles([]ListItem{})
 	grid.AddItem(tab2, 1, 0, 1, 1, 0, 0, false)
 
 	// Row 2 Column 2
-	tab3 := buildPeople([]ListItem{})
+	tab3 = buildPeople([]ListItem{})
 	grid.AddItem(tab3, 1, 1, 1, 1, 0, 0, false)
 
 	// Row 3 Column 1-2
@@ -76,5 +81,24 @@ func DrawPanel() Panel {
 		Tab1: tab1,
 		Tab2: tab2,
 		Tab3: tab3,
+	}
+}
+
+func onSearched(key tcell.Key) {
+	if key == tcell.KeyEnter {
+		tab2.Clear()
+		tab3.Clear()
+
+		textInput := tab1.GetText()
+		titles, people := SearchImdb(textInput)
+
+		// Since range doesn't guarantee the order, for loop is best here
+		for index := 0; index < len(titles); index++ {
+			tab2.AddItem(titles[index].Name, titles[index].Summary, rune('0'+index), nil)
+		}
+
+		for index := 0; index < len(people); index++ {
+			tab3.AddItem(people[index].Name, people[index].Summary, rune('0'+index), nil)
+		}
 	}
 }
