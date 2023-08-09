4e9b602271356f652068b3df77cf1950a7f11a76
fkolcu <furkankolcu@hotmail.com.tr>
2023-08-09 10:55:42 +0300

    add github link to footer

--- internal/view/footer.go
+++ internal/view/footer.go
@@ -1,6 +1,10 @@
 package view
 
-import "github.com/rivo/tview"
+import (
+	"github.com/fkolcu/imdb-terminal/internal/util"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
 
 const (
 	bindingText = "[blue]<esc>: Quit, <tab>: Jump between tabs/buttons, <enter>: Select"
@@ -14,6 +18,15 @@ func NewFooter() *tview.Flex {
 	keybindingText := NewText(TextConfig{DynamicColors: true})
 	keybindingText.SetText(bindingText)
 	myFooter.AddItem(keybindingText, 0, 1, false)
+
+	githubLinkText := NewText(TextConfig{})
+	githubLinkText.SetText("imdb-terminal")
+	githubLinkText.SetBackgroundColor(tcell.ColorYellow)
+	githubLinkText.SetTextColor(tcell.ColorBlack)
+	githubLinkText.SetFocusFunc(func() {
+		util.OpenUrl("https://github.com/fkolcu/imdb-terminal")
+	})
+	myFooter.AddItem(githubLinkText, 13, 1, false)
 
 	// more to come later
 
